a2e86e159eb4b78114e817e574fcfd2dbaab1f7c
Elliot Winkler <elliot.winkler@gmail.com>
2010-02-10 17:41:22 -0600

    Get rid of the mime extensions, they're not really necessary

--- lib/mcmire/render_htmldoc_pdf/mime_ext.rb
+++
@@ -1,8 +0,0 @@
-# Convenience method to get the MIME type for an extension
-module Mime
-  class Type
-    def self.for(symbol)
-      mime = EXTENSION_LOOKUP[symbol.to_s] && mime.to_s
-    end
-  end
-end
